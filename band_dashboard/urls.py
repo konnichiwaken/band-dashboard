3f3949522093393e47d31887626eaac210ebdf41
Ken McAndrews <ken.mcandrews@gmail.com>
2015-10-25 17:49:50 -0700

    Ability to login

--- band_dashboard/urls.py
+++ band_dashboard/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet
+from authentication.views import LoginView
 from band_dashboard.views import IndexView
 
 
@@ -27,8 +28,7 @@ router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
      '',
-    # ... URLs
     url(r'^api/v1/', include(router.urls)),
-
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
--- static/javascripts/authentication/register.controller.js
+++ static/javascripts/authentication/register.controller.js
@@ -19,13 +19,52 @@
 
     vm.register = register;
 
+    activate();
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf thinkster.authentication.controllers.RegisterController
+     */
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      if (Authentication.isAuthenticated()) {
+        $location.url('/');
+      }
+    }
+
     /**
     * @name register
-    * @desc Register a new user
-    * @memberOf thinkster.authentication.controllers.RegisterController
+    * @desc Try to register a new user
+    * @param {string} email The email entered by the user
+    * @param {string} password The password entered by the user
+    * @param {string} username The username entered by the user
+    * @returns {Promise}
+    * @memberOf thinkster.authentication.services.Authentication
     */
-    function register() {
-      Authentication.register(vm.email, vm.password, vm.first_name, vm.last_name);
+    function register(email, password, first_name, last_name) {
+      return $http.post('/api/v1/accounts/', {
+        email: email,
+        password: password,
+        first_name: first_name,
+        last_name: last_name
+      }).then(registerSuccessFn, registerErrorFn);
+
+      /**
+      * @name registerSuccessFn
+      * @desc Log the new user in
+      */
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      /**
+      * @name registerErrorFn
+      * @desc Log "Epic failure!" to the console
+      */
+      function registerErrorFn(data, status, headers, config) {
+        console.error('Error when registering');
+      }
     }
   }
 })();
