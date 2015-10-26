807f4dd01b5f77d8e23d44f6c11b1e05652ab119
Ken McAndrews <ken.mcandrews@gmail.com>
2015-10-25 17:32:58 -0700

    Ability to register users

---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('band-dash.authentication', [
+      'ngCookies'
+    ]);
+})();
--- static/javascripts/band-dash.js
+++ static/javascripts/band-dash.js
@@ -1,2 +1,31 @@
-angular
-  .module('band-dash', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('band-dash', [
+      'band-dash.config',
+      'band-dash.routes',
+      'band-dash.authentication'
+    ]);
+
+  angular
+    .module('band-dash.routes', ['ngRoute']);
+
+  angular
+    .module('band-dash.config', []);
+
+  angular
+    .module('band-dash')
+    .run(run);
+
+  run.$inject = ['$http'];
+
+  /**
+  * @name run
+  * @desc Update xsrf $http headers to align with Django's defaults
+  */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
