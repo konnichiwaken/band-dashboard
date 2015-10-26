a577669f377a5c7e6d3812ec8e5a8283eeaa1480
Ken McAndrews <ken.mcandrews@gmail.com>
2015-10-25 17:49:50 -0700

    Ability to login

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,9 @@
+import json
+
+from django.contrib.auth import authenticate, login
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework import views
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -36,3 +40,32 @@ class AccountViewSet(viewsets.ModelViewSet):
             'status': 'Bad request',
             'message': 'Account could not be created with received data.'
         }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Email/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
--- static/javascripts/authentication/authentication.service.js
+++ static/javascripts/authentication/authentication.service.js
@@ -21,7 +21,12 @@
     * @desc The Factory to be returned
     */
     var Authentication = {
-      register: register
+      register: register,
+      login: login,
+      getAuthenticatedAccount: getAuthenticatedAccount,
+      isAuthenticated: isAuthenticated,
+      setAuthenticatedAccount: setAuthenticatedAccount,
+      unauthenticate: unauthenticate
     };
 
     return Authentication;
@@ -36,7 +41,7 @@
     * @param {string} first_name The first name entered by the user
     * @param {string} last_name The last name entered by the user
     * @returns {Promise}
-    * @memberOf thinkster.authentication.services.Authentication
+    * @memberOf thinkster.authentication.Authentication
     */
     function register(email, password, first_name, last_name) {
       return $http.post('/api/v1/accounts/', {
@@ -45,6 +50,84 @@
         first_name: first_name,
         last_name: last_name
       });
+    }
+
+    /**
+     * @name login
+     * @desc Try to log in with email `email` and password `password`
+     * @param {string} email The email entered by the user
+     * @param {string} password The password entered by the user
+     * @returns {Promise}
+     * @memberOf thinkster.authentication.Authentication
+     */
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email,
+        password: password
+      }).then(loginSuccessFn, loginErrorFn);
+
+      /**
+       * @name loginSuccessFn
+       * @desc Set the authenticated account and redirect to index
+       */
+      function loginSuccessFn(data, status, headers, config) {
+        Authentication.setAuthenticatedAccount(data.data);
+
+        window.location = '/';
+      }
+
+      /**
+       * @name loginErrorFn
+       * @desc Log "Epic failure!" to the console
+       */
+      function loginErrorFn(data, status, headers, config) {
+        console.error('Login failure');
+      }
+    }
+
+    /**
+     * @name getAuthenticatedAccount
+     * @desc Return the currently authenticated account
+     * @returns {object|undefined} Account if authenticated, else `undefined`
+     * @memberOf thinkster.authentication.Authentication
+     */
+    function getAuthenticatedAccount() {
+      if (!$cookies.authenticatedAccount) {
+        return;
+      }
+
+      return JSON.parse($cookies.authenticatedAccount);
+    }
+
+    /**
+     * @name isAuthenticated
+     * @desc Check if the current user is authenticated
+     * @returns {boolean} True is user is authenticated, else false.
+     * @memberOf thinkster.authentication.Authentication
+     */
+    function isAuthenticated() {
+      return !!$cookies.authenticatedAccount;
+    }
+
+    /**
+     * @name setAuthenticatedAccount
+     * @desc Stringify the account object and store it in a cookie
+     * @param {Object} user The account object to be stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.Authentication
+     */
+    function setAuthenticatedAccount(account) {
+      $cookies.authenticatedAccount = JSON.stringify(account);
+    }
+
+    /**
+     * @name unauthenticate
+     * @desc Delete the cookie where the user object is stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.Authentication
+     */
+    function unauthenticate() {
+      delete $cookies.authenticatedAccount;
     }
   }
 })();
