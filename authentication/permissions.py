4b88cf0b1b11094595a28319d42a3e22733a1c74
Ken McAndrews <ken.mcandrews@gmail.com>
2016-02-18 23:31:21 -0800

    Add permissions to account creation

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,9 +1,23 @@
 from rest_framework import permissions
 
+from authentication.utils import is_account_creation_admin
+
 
 class IsAccountOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, account):
-        if request.user:
+        if request.method == 'PUT':
             return account == request.user
+
+        return False
+
+
+class CanCreateAccount(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return is_account_creation_admin(request.user)
+        elif request.method == 'PUT':
+            return True
+
         return False
