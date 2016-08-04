4c042d90ec13fc338d8de6fc913d0ec34f16645b
Ken McAndrews <ken.mcandrews@gmail.com>
2016-08-04 00:56:16 -0400

    Placeholder function for sending registration email

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from itsdangerous import URLSafeTimedSerializer
+
 from authentication.settings import ACCOUNT_CREATION_ADMIN_ROLES
 
 
@@ -6,5 +9,23 @@ def is_account_creation_admin(account):
     return bool(roles.intersection(ACCOUNT_CREATION_ADMIN_ROLES))
 
 
-def create_account(account):
-    print account
+def send_registration_email(account):
+    token = generate_confirmation_token(account.email)
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    return serializer.dumps(email, salt=settings.SECURITY_PASSWORD_SALT)
+
+
+def confirm_token(token, expiration=86400):
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    try:
+        email = serializer.loads(
+            token,
+            salt=settings.SECURITY_PASSWORD_SALT,
+            max_age=expiration)
+    except:
+        return False
+
+    return email
