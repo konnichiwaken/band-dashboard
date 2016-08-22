b47fe54162fda11a622fe2beb40dfe8f1de3ff34
Ken McAndrews <ken.mcandrews@gmail.com>
2016-08-21 23:28:42 -0700

    Only send emails on PROD

--- emails/tasks.py
+++ emails/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 
+from django.conf import settings
+
 from .models import Email
 from .utils import EmailServer
 
@@ -7,10 +9,15 @@ from .utils import EmailServer
 @shared_task
 def send_unsent_emails():
     emails = Email.objects.filter(is_sent=False)
-    email_server = EmailServer()
+    if settings.ENV != "DEV":
+        email_server = EmailServer()
+
     for email in emails:
-        email_server.send_email(email.recipient, email.body, email.subject)
+        if settings.ENV != "DEV":
+            email_server.send_email(email.recipient, email.body, email.subject)
+
         email.is_sent = True
         email.save()
 
-    email_server.tear_down()
+    if settings.ENV != "DEV":
+        email_server.tear_down()
